a089507266497edcb902b9d9456fd2becf4b37ea
CyberNinja600 <74649503+CyberNinja600@users.noreply.github.com>
2024-08-03 13:47:39 +0600

    update

--- src/components/Auth/registration-form.jsx
+++ src/components/Auth/registration-form.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
 const RegistrationForm = () => {
-  return <div>RegistrationForm</div>;
+  return (
+
+      
+      <div className='w-auto h-auto lg:w-[471px] lg:h-[753px] p-4 alternative-color space-y-4 flex justify-center'>
+        <div className='space-y-4 pt-14'>
+          <div className='music-library-barcode text-white w-full text-center'>
+              MusicLibrary
+          </div>
+
+          <div className='flex flex-col w-[382px] '>
+            <label htmlFor="email" className='auth-form-label text-[#6d6d6d]'>Email</label>
+            <input className='bg-black h-12 p-2 rounded-[10px] ' name="email" type="text" placeholder='Email' />
+          </div>
+
+          <div className='flex flex-col'>
+            <label htmlFor="password" className='auth-form-label text-[#6d6d6d]'>Password</label>
+            <input className='bg-black h-12 p-2 rounded-[10px]' name="password" type="password" placeholder='Password'/>
+          </div>
+
+          <div className='flex flex-col'>
+            <label htmlFor="confirm_password" className='auth-form-label text-[#6d6d6d]'>Confirm password</label>
+            <input className='bg-black h-12 p-2 rounded-[10px]' name="confirm_password" type="password" placeholder='Confirm Password'/>
+          </div>
+
+        </div>
+      </div>
+  );
 };
 
 export default RegistrationForm;
--- src/pages/Auth/RegistrationPage.jsx
+++ src/pages/Auth/RegistrationPage.jsx
@@ -3,8 +3,9 @@ import RegistrationForm from '../../components/auth/registration-form';
 
 const RegistrationPage = () => {
   return (
-    <div>
-      Registration
+    <div className='bg-neutral-950 h-screen grid place-content-center'>
+      <link href='https://fonts.googleapis.com/css?family=Libre Barcode 128 Text' rel='stylesheet'></link>
+      <link href='https://fonts.googleapis.com/css?family=Battambang' rel='stylesheet'></link>
       <RegistrationForm />
     </div>
   );
