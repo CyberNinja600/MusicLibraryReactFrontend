4e8878814de5f2ee8f4a03fff33632b4f15ea414
CyberNinja600 <74649503+CyberNinja600@users.noreply.github.com>
2024-08-04 04:05:09 +0600

    Feat: Login Form Design

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import RegistrationPage from '../pages/Auth/RegistrationPage';
+import LoginPage from '../pages/Auth/LoginPage';
 import HomePage from '../pages/Homepage';
 
 const AppRoutes = () => {
   return (
     <Router>
+
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
+
       <Routes>
         <Route path="/register" element={<RegistrationPage />} />
+      </Routes>
+
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
