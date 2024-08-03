6109a3d6a5e49a3891637b53f8a823f428d3883a
CyberNinja600 <74649503+CyberNinja600@users.noreply.github.com>
2024-08-03 16:00:42 +0600

    feat: login page design v1.000

--- src/components/Auth/registration-form.jsx
+++ src/components/Auth/registration-form.jsx
@@ -4,25 +4,35 @@ const RegistrationForm = () => {
   return (
 
       
-      <div className='w-auto h-auto lg:w-[471px] lg:h-[753px] p-4 alternative-color space-y-4 flex justify-center'>
-        <div className='space-y-4 pt-14'>
+      <div className='w-auto h-auto lg:w-[471px] lg:h-[753px] p-4 bg-black space-y-4 flex justify-center'>
+        <div className='space-y-4 pt-14 '>
           <div className='music-library-barcode text-white w-full text-center'>
               MusicLibrary
           </div>
 
-          <div className='flex flex-col w-[382px] '>
-            <label htmlFor="email" className='auth-form-label text-[#6d6d6d]'>Email</label>
-            <input className='bg-black h-12 p-2 rounded-[10px] ' name="email" type="text" placeholder='Email' />
+          <div className='flex flex-col w-[382px] floating-label-content border-neutral-500 border-[1px] rounded-sm '>
+            <input className='  text-neutral-200 floating-input' name="email" type="text" placeholder='Email' />
+            <label htmlFor="email" className='py-1 px-1 text-[#6d6d6d] floating-label '>Email</label>
           </div>
 
-          <div className='flex flex-col'>
-            <label htmlFor="password" className='auth-form-label text-[#6d6d6d]'>Password</label>
-            <input className='bg-black h-12 p-2 rounded-[10px]' name="password" type="password" placeholder='Password'/>
+          <div className='flex flex-col w-[382px] floating-label-content border-neutral-500 border-[1px] rounded-sm'>
+            <input className='  text-neutral-200 floating-input' name="password" type="password" placeholder='Password'/>
+            <label htmlFor="password" className='py-1 px-1 text-[#6d6d6d] floating-label '>Password</label>
           </div>
 
-          <div className='flex flex-col'>
-            <label htmlFor="confirm_password" className='auth-form-label text-[#6d6d6d]'>Confirm password</label>
-            <input className='bg-black h-12 p-2 rounded-[10px]' name="confirm_password" type="password" placeholder='Confirm Password'/>
+          <div className='flex flex-col floating-label-content border-neutral-500 border-[1px] rounded-sm'>
+            <input className='  text-neutral-200 floating-input' name="confirm_password" type="password" placeholder='Confirm Password'/>
+            <label htmlFor="confirm_password" className='py-1 px-1 text-[#6d6d6d] floating-label '>Confirm password</label>
+          </div>
+
+          <div className='text-neutral-500' name='login'>
+              Already have an account? Sign In
+          </div>
+
+          <div name="register" className="border-cyan-300 border-2 flex justify-center ">
+              <button className='p-4 text-cyan-300'>
+                Register
+              </button>
           </div>
 
         </div>
