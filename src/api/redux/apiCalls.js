f3615f1402f19695708cb45aff70cf06026be5d1
CyberNinja600 <74649503+CyberNinja600@users.noreply.github.com>
2024-10-14 21:41:22 +0600

    feat: login error handle

--- src/api/redux/apiCalls.js
+++ src/api/redux/apiCalls.js
@@ -3,20 +3,21 @@ import { loginFailure, loginStart, loginSuccess, logout } from "./userRedux";
 
 
 export const login = async (dispatch, userCredentials) => {
-    dispatch(loginStart());
-    try {
-      const res = await publicRequest.post('/login', userCredentials);
-      dispatch(loginSuccess(res.data));
-    } catch (error) {
-      if (error.response && error.response.status === 401) {
-        throw new Error('Invalid email or password'); 
-      }
+  dispatch(loginStart());
+  try {
+    const res = await publicRequest.post('/login', userCredentials);
+    dispatch(loginSuccess(res.data));
+  } catch (error) {
+    if (error.response && error.response.status === 403) {
+      // throw new Error('Invalid email or password');
+      alert("Invalid email")
       dispatch(loginFailure());
-      throw error;
     }
-  };
-
-  export const logoutApi = async (dispatch) => { 
-    await publicRequest.post('/logout');
-    dispatch(logout());
+    // throw error;
   }
+};
+
+export const logoutApi = async (dispatch) => { 
+  await publicRequest.post('/logout');
+  dispatch(logout());
+}
