4f94ca2b76a137874b466adc1dc6100e48eb424b
CyberNinja600 <74649503+CyberNinja600@users.noreply.github.com>
2024-11-05 11:39:52 +0600

    feat: structure homepage

---
+++ src/components/main/main-menu.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MainMenu = () => {
+    return (
+        <div name="main_menu" className='h-[84%] flex'>
+            <div name="your_music" className='w-[15%] bg-cyan-300'>
+                This is your music
+            </div>
+
+            <div name="home_page" className='w-[85%] bg-cyan-200'>
+                This is home_page
+            </div>
+      </div>
+    );
+}
+
+export default MainMenu;
---
+++ src/components/musicPlayer/music-player-shell.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const MusicPlayerShell = () => {
+    return (
+        <div name="music_player" className='bg-neutral-500 h-[10%]'>
+        Moozik pLeya
+      </div>
+    );
+};
+
+
+export default MusicPlayerShell;
