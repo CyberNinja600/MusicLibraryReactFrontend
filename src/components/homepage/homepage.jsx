34a90223b11fb768e55a16c85717fe2fe77124c4
CyberNinja600 <74649503+CyberNinja600@users.noreply.github.com>
2024-12-03 20:31:10 +0600

    home page gridify

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,25 +1,45 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const Homepage = ({allAlbums}) => {
   const albums = allAlbums
-  console.log(albums)
+  console.log(albums)  
+  const currentUser = useSelector((state) => state.user.currentUser);
+
 
   const renderAlbums = () => {
     return Object.keys(albums).map((key, index) => (
       <div key={index}>
-        <h3>Album: {key}</h3>
-        <p>Details: {JSON.stringify(albums[key])}</p>
+        <div className=' h-56 w-52 justify-center flex bg-neutral-900 rounded-lg'>
+            <div className='justify-center items-center flex flex-col'>
+              <img className='pt-1 object-fit h-[184px] w-[184px]' src={albums[key].image_url} alt=""/>
+              <div className='truncate w-[90%] text-neutral-300'>
+                {albums[key].name}
+              </div>
+            </div>
+        </div>
       </div>
+      
     ));
   };
+  
 
   return (
-    <div>
-      {albums && Object.keys(albums).length > 0 ? (
-        renderAlbums()
-      ) : (
-        <p>No albums available.</p>
-      )}
+    <div className='h-full bg-neutral-900 rounded-lg mr-2 sm:p-2 sm:pt-4 z-0 overflow-clip'>
+        <div name="card" className='h-full mx-3 overflow-y-scroll your-playlist-scroll'>
+            <div name="title" className='font-bold text-neutral-200 text-2xl'>
+                  Made for {currentUser.data.name}
+            </div>
+            <div class="grid gap-4 grid-cols-[repeat(auto-fit,minmax(200px,1fr))] sm:pt-4">
+              {/* loop starts here */}
+              {albums && Object.keys(albums).length > 0 ? (
+                renderAlbums()
+              ) : (
+                <p>No albums available.</p>
+              )}
+
+          </div>
+        </div>
     </div>
   );
 };
