b88ece3605e01b8b9dca2302d7b1c50e08ce913c
CyberNinja600 <74649503+CyberNinja600@users.noreply.github.com>
2024-12-03 20:32:04 +0600

    hover: color change

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -10,10 +10,10 @@ const Homepage = ({allAlbums}) => {
   const renderAlbums = () => {
     return Object.keys(albums).map((key, index) => (
       <div key={index}>
-        <div className=' h-56 w-52 justify-center flex bg-neutral-900 rounded-lg'>
+        <div className=' h-56 w-52 justify-center flex hover:bg-neutral-800 rounded-lg'>
             <div className='justify-center items-center flex flex-col'>
               <img className='pt-1 object-fit h-[184px] w-[184px]' src={albums[key].image_url} alt=""/>
-              <div className='truncate w-[90%] text-neutral-300'>
+              <div className='truncate w-[90%] text-neutral-300 antialiased'>
                 {albums[key].name}
               </div>
             </div>
