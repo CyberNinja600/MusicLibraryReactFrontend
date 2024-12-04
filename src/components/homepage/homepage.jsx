c45b47e70a264dcb0f5bfe83aed785731f657645
CyberNinja600 <74649503+CyberNinja600@users.noreply.github.com>
2024-12-04 16:16:02 +0600

    fix scroll color, manage session with token

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,25 +1,15 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import AlbumItem from './album-item';
 
 const Homepage = ({allAlbums}) => {
   const albums = allAlbums
-  console.log(albums)  
   const currentUser = useSelector((state) => state.user.currentUser);
 
 
   const renderAlbums = () => {
     return Object.keys(albums).map((key, index) => (
-      <div key={index}>
-        <div className=' h-56 w-52 justify-center flex hover:bg-neutral-800 rounded-lg'>
-            <div className='justify-center items-center flex flex-col'>
-              <img className='pt-1 object-fit h-[184px] w-[184px]' src={albums[key].image_url} alt=""/>
-              <div className='truncate w-[90%] text-neutral-300 antialiased'>
-                {albums[key].name}
-              </div>
-            </div>
-        </div>
-      </div>
-      
+      <AlbumItem index={index} albumData={albums[key]}/>
     ));
   };
   
@@ -30,12 +20,12 @@ const Homepage = ({allAlbums}) => {
             <div name="title" className='font-bold text-neutral-200 text-2xl'>
                   Made for {currentUser.data.name}
             </div>
-            <div class="grid gap-4 grid-cols-[repeat(auto-fit,minmax(200px,1fr))] sm:pt-4">
+            <div className="grid gap-4 grid-cols-[repeat(auto-fit,minmax(200px,1fr))] sm:pt-4">
               {/* loop starts here */}
               {albums && Object.keys(albums).length > 0 ? (
                 renderAlbums()
               ) : (
-                <p>No albums available.</p>
+                <p className="text-gray-500 text-center">No albums found.</p>
               )}
 
           </div>
